d7abf41390873499075219f7826de37b314091e8
Sahat Renaldi S <renaldi.crk@gmail.com>
2026-01-20 09:40:36 +0700

    Update code

--- db.py
+++ db.py
@@ -4,13 +4,16 @@ import streamlit as st
 
 @st.cache_data
 def fetch_dataframe(query, params=None):
-    engine = create_engine(st.secrets["DATABASE_URL"])
+    db = st.secrets["db"]
+
+    DATABASE_URL = (
+        f"postgresql+psycopg2://{db['user']}:{db['password']}"
+        f"@{db['host']}:{db['port']}/{db['dbname']}"
+    )
+
+    engine = create_engine(DATABASE_URL)
 
     with engine.connect() as conn:
-        df = pd.read_sql(
-            text(query),   
-            conn,
-            params=params
-        )
+        df = pd.read_sql(text(query), conn, params=params)
 
     return df
